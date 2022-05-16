600edfa074b4e24f09de3bedc3b3eaec403dec2e
Thiago Sant Helena <21184066+shthiago@users.noreply.github.com>
2022-05-16 18:47:15 -0300

    Make detection of primary only chain of expressions detect over any expression

--- transpiler/expression_handler.py
+++ transpiler/expression_handler.py
@@ -1,41 +1,94 @@
-from typing import Optional
+from typing import Optional, Union
 
 from transpiler.structures.nodes.expression import (
+    AdditiveExpression,
+    AndExpression,
+    MultiplicativeExpression,
     OrExpression,
     PrimaryExpression,
     PrimaryType,
+    RelationalExpression,
+    UnaryExpression,
 )
 
 
 class ExpressionHandler:
-    def primary_only(self, node: OrExpression) -> Optional[PrimaryExpression]:
-        """Check if a given OrExpression have only a primary"""
+    def primary_only(
+        self,
+        node: Union[
+            OrExpression,
+            AndExpression,
+            RelationalExpression,
+            AdditiveExpression,
+            MultiplicativeExpression,
+            UnaryExpression,
+        ],
+    ) -> Optional[PrimaryExpression]:
+        """Check if a given expression have only a primary"""
+        node_type = type(node).__name__
+        match node_type:
+            case "OrExpression":
+                return self._primary_only_orexpression(node)
+            case "AndExpression":
+                return self._primary_only_andexpression(node)
+            case "RelationalExpression":
+                return self._primary_only_relationalexpression(node)
+            case "AdditiveExpression":
+                return self._primary_only_additiveexpression(node)
+            case "MultiplicativeExpression":
+                return self._primary_only_multiplicativeexpression(node)
+            case "UnaryExpression":
+                return self._primary_only_unaryexpression(node)
+            case _:
+                raise Exception(f"Unexpected type: {node_type}")
+
+    def _primary_only_unaryexpression(
+        self, node: UnaryExpression
+    ) -> Optional[PrimaryExpression]:
+        if node.op:
+            return None
+
+        return node.value
+
+    def _primary_only_multiplicativeexpression(
+        self, node: MultiplicativeExpression
+    ) -> Optional[PrimaryExpression]:
         if node.others:
             return None
 
-        and_node = node.base
+        return self._primary_only_unaryexpression(node.base)
 
-        if and_node.others:
+    def _primary_only_additiveexpression(
+        self, node: AndExpression
+    ) -> Optional[PrimaryExpression]:
+        if node.others:
             return None
 
-        rel_node = and_node.base
+        return self._primary_only_multiplicativeexpression(node.base)
 
-        if rel_node.second:
+    def _primary_only_relationalexpression(
+        self, node: RelationalExpression
+    ) -> Optional[PrimaryExpression]:
+        if node.second:
             return None
 
-        add_node = rel_node.first
+        return self._primary_only_additiveexpression(node.first)
 
-        if add_node.others:
+    def _primary_only_andexpression(
+        self, node: AndExpression
+    ) -> Optional[PrimaryExpression]:
+        if node.others:
             return None
 
-        mult_node = add_node.base
+        return self._primary_only_relationalexpression(node.base)
 
-        if mult_node.others:
+    def _primary_only_orexpression(
+        self, node: OrExpression
+    ) -> Optional[PrimaryExpression]:
+        if node.others:
             return None
 
-        unary_node = mult_node.base
-
-        return unary_node.value
+        return self._primary_only_andexpression(node.base)
 
     def is_literal(self, node: OrExpression) -> bool:
         primary = self.primary_only(node)
